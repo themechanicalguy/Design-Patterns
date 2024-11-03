511f99f5d4b433a4f352f26c6903a9e7c8a55704
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2024-11-03 12:00:25 +0530

    structural init

---
+++ 3-Structural/1-decorator.js
@@ -0,0 +1,7 @@
+/**
+ * The Decorator Design Pattern is a structural design pattern that allows behavior to be added to individual objects dynamically, 
+    without affecting the behavior of other objects from the same class. 
+ * It involves creating a set of decorator classes that are used to wrap concrete components.
+ * This pattern is useful when you need to add functionality to objects in a flexible and reusable way.
+ * This pattern is vital for enhancing functionality while adhering to the open-closed principle.
+ */
